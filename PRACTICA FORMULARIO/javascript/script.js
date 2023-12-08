d849d039416cdbb3445383dfb29e6392f3375340
rzk94 <elizabeth.razok@gmail.com>
2023-12-08 17:30:06 -0600

    Primeros comando de Javascript

---
+++ PRACTICA FORMULARIO/javascript/script.js
@@ -0,0 +1,60 @@
+//Comentarios de una sola linea
+/*comentarios 
+miltilineas*/
+
+// alert("Hola mundo");
+// console.log("Este es un mensaje para consola");
+
+
+// var nombre=prompt("ingresa tu nombre");
+// alert("Hola, "+nombre+" bienvenide a la CH35.");
+
+
+/*var numero1 = parseInt(prompt("Ingresa el primer numero"));
+var numero2 = parseInt(prompt("Ingresa el segundo numero"));
+// console.log("La suma de "+numero1+"+"+numero2+"="+(numero1+numero2));
+console.log("suma: "+(numero1 + numero2));
+console.log("resta: "+(numero1 - numero2));
+console.log("multiplicacion: "+(numero1 * numero2));
+console.log("division: "+(numero1 / numero2));*/
+
+
+// var edad=prompt("Ingresa tu edad: ");
+
+// if(edad<18){
+//     alert("No eres legal.");
+// }
+// if(edad>=18&edad<30){
+//     alert("Todavia estas chavx");
+// }
+// if(edad>=30){
+//     alert("Ya estas mas cerca del panteon");
+// }
+// return edad;
+
+//TIPOS DE DATOS Y VARIABLES
+
+//scope es el alcance de nuestras variables
+
+/*Var, let, const la diferencia es el alcance
+var tiene un alcance global
+let tiene un alcance local
+const se mantiene sin ningun cambio
+*/
+
+/*string: cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied: no esta asignado o definido
+ */
+
+
+let nombre = "Daniel";
+let invitadosextra = 2;
+let esmayoredad = true;
+let edad = 24;
+let invitadoespecial = null;
+let horadesalida = undefined;
+
+
